1fd5a4bb33e92e3deb211a5d3ba0112b0f6d7fd7
Christian Gonzalez <kgomin@gmail.com>
2022-02-10 17:29:06 +0100

    Lab solved

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const mongoose = require('mongoose');
 
 // Import of the model Recipe from './models/Recipe.model.js'
@@ -5,7 +7,7 @@ const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
 const data = require('./data');
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = `mongodb+srv://${process.env.MG_USERNAME}:${process.env.MG_PWD}@fatchancho.1vmh5.mongodb.net/recipe-app?retryWrites=true&w=majority`;
 
 // Connection to the database "recipe-app"
 mongoose
@@ -13,11 +15,19 @@ mongoose
   .then(x => {
     console.log(`Connected to the database: "${x.connection.name}"`);
     // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
+    //return Recipe.deleteMany()
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+   Recipe
+  //.insertMany(data)
+  //.then((data)=>data.forEach((recipe)=>console.log(recipe.title)))
+  //.findOneAndUpdate({title:{$eq:'Rigatoni alla Genovese'}},{$set:{duration:100}},{new:true})
+  //.then((recipe)=>console.log(recipe));
+  //.deleteOne({title:{$eq:'Carrot Cake'}})
   })
+  .then(mongoose.connection.close())
+
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+
